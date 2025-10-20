fb6670f182f8c54b81cc9214340608b98ac14174
ShawtyMeal <110601259+ShawtyMeal@users.noreply.github.com>
2025-10-20 09:52:07 +0800

    calculator interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      JR Initial Commit
-    </div>
-  );
-}
 
+const App  = () => {
+    const [addition, setAddition] = useState("")
+    
+    const handleClick = (value: string) => {
+      const operator = ['+', '-', '*', '/'];
+      if (operator.includes(value)) {
+        if (addition === "") return;
+
+        const lastChar = addition [addition.length -1];
+        if(operator.includes(lastChar)) return;
+      }
+      setAddition((prev) => prev + value);
+
+    };
+
+    const handleEqual = () => {
+      try {
+        const result = eval(addition.replace(/x/g, '*'));
+        setAddition(String(result));
+      } catch (error) {
+        setAddition("Error");
+      }
+    };
+    const handleClear = () => {
+  setAddition("");
+};
+
+    return(
+     <div className="App">
+    <div className="calc-wrapper">
+    <input type="text" value={addition} placeholder="
+     Enter number" readOnly />
+
+    <div className="row">
+    <button className="button" onClick={handleClear}>C</button>
+    <button className="button" onClick={() => (handleClick("%"))}>%</button>
+    <button className="button" disabled>:)</button>
+    <button className="button"  disabled>:(</button>
+    </div>
+    <div className="row">
+    <button className="button" onClick={() => (handleClick("7"))}>7</button>
+    <button className="button" onClick={() => (handleClick("8"))}>8</button>
+    <button className="button" onClick={() => (handleClick("9"))}>9</button>
+    <button className="button" onClick={() => (handleClick("*"))}>*</button>
+    </div>
+    <div className="row">
+    <button className="button" onClick={() => (handleClick("4"))}>4</button>
+    <button className="button" onClick={() => (handleClick("5"))}>5</button>
+    <button className="button" onClick={() => (handleClick("6"))}>6</button>
+    <button className="button" onClick={() => (handleClick("-"))}>-</button>
+    </div>
+     <div className="row">
+    <button className="button" onClick={() => (handleClick("1"))}>1</button>
+    <button className="button" onClick={() => (handleClick("2"))}>2</button>
+    <button className="button" onClick={() => (handleClick("3"))}>3</button>
+    <button className="button" onClick={() => (handleClick("+"))}>+</button>
+    </div>
+    <div className="row">
+    <button className="button" onClick={() => (handleClick("/"))}>/</button>
+    <button className="button" onClick={() => (handleClick("0"))}>0</button>
+    <button className="button" onClick={() => (handleClick("."))}>.</button>
+    <button className="button" onClick={handleEqual}>=</button>
+    </div>
+      </div>
+      </div>
+    )
+}
 export default App;
